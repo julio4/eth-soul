b252c1bf75a51c4f88b5f65dbad86ded9c197e31
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-10 11:48:05 +0200

    Rating component

---
+++ packages/frontend/src/components/profile/Rating.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import { AiFillStar } from 'react-icons/ai';
+import { useColorModeValue, Flex, Text } from '@chakra-ui/react';
+import { RatingType } from '@types/app';
+
+type RatingProps = {
+    rating: RatingType
+}
+
+export const Rating: FC<RatingProps> = ({ rating }) => {
+    return (
+        <Flex alignItems="center" justifyContent="flex-start" width="100%" align="center" alignContent={"center"} mb={2} ml={-1}>
+            <AiFillStar color="yellow" size={22} />
+            <Text fontWeight="bold" color="gray.900" pl={1}>{rating.average}/5</Text>
+            <Text fontWeight="hairline" color="gray.600" pl={1}>({rating.numberOfReviews} reviews)</Text>
+        </Flex>
+    );
+}
