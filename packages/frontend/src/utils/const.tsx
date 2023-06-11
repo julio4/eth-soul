a5b3ec95925ef2c7bb0b758bb34a2ce677928e9a
user <clementjuventin@gmail.com>
2023-06-11 10:10:50 +0200

    feat(): populate and updates

--- packages/frontend/src/utils/const.tsx
+++ packages/frontend/src/utils/const.tsx
@@ -1,4 +1,4 @@
-const CONTRACT_ADDRESS = '0x627665fE893314D4e18ed87F2510EaF85CaF8530'
-const THE_GRAPH_URL = 'https://api.studio.thegraph.com/query/48177/sepoliafinal/0.1.1'
+const CONTRACT_ADDRESS = '0x9F8865559f2b22F3883e162bEbe80F6069Dd9Dc9'
+const THE_GRAPH_URL = 'https://api.studio.thegraph.com/query/48177/sepoliafinal/version/latest'
 
 export { CONTRACT_ADDRESS, THE_GRAPH_URL }
